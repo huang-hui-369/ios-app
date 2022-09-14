3a059035ba3e8f00ecdd51f102df2abc653ffa96
huang <huanghui@grandunit.net>
2022-09-15 00:56:31 +0900

    纯手写ios app uikit 模版

---
+++ uikitdemo1/uikitdemo1/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  uikitdemo1
+//
+//  Created by huang hui on 2022/09/15.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+        return true
+    }
+
+}
+
---
+++ uikitdemo1/uikitdemo1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uikitdemo1
+//
+//  Created by huang hui on 2022/09/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.yellow
+    }
+
+
+}
+
